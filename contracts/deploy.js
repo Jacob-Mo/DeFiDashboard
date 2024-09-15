c632ff0a29b4ab3f4d0e95608bd07044a9b8591c
Jacob-Mo <brucelee29373@gmail.com>
2024-09-15 13:06:21 +0200

    added new function

--- contracts/deploy.js
+++ contracts/deploy.js
@@ -7,16 +7,35 @@ console.log('Deploying with the following configuration:');
 console.log(`DB User: ${dbUser}`);
 console.log(`DB Password: ${dbPassword}`);
 
+function simulateDatabaseConnection(dbUser, dbPassword, callback) {
+    setTimeout(() => {
+        if (dbUser && dbPassword) {
+            console.log('Database connection successful.');
+            callback(true);
+        } else {
+            console.log('Database connection failed. Check configuration.');
+            callback(false);
+        }
+    }, 1000);
+}
+
 function deploy() {
     if (!dbUser || !dbPassword) {
         console.error('Missing environment variables. Deployment aborted.');
         return;
     }
 
-    console.log('Deployment in progress...');
-    setTimeout(() => {
-        console.log('Deployment successful.');
-    }, 2000);
+    simulateDatabaseConnection(dbUser, dbPassword, (isConnected) => {
+        if (!isConnected) {
+            console.error('Deployment aborted due to database connection failure.');
+            return;
+        }
+
+        console.log('Deployment in progress...');
+        setTimeout(() => {
+            console.log('Deployment successful.');
+        }, 2000);
+    });
 }
 
 deploy();
